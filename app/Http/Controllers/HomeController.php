9554e8a9eaba2b93cf3014c92e5e9108d89457a0
Miguel Angel Lara Garcia <miguelangellaragarcia@MacBook-Pro-de-Miguel.local>
2021-03-26 19:06:08 +0100

    terminacion punto 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Announcement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 use App\Http\Requests\AnnouncementRequest;
 
 class HomeController extends Controller{
@@ -11,6 +13,9 @@ class HomeController extends Controller{
   public function __construct()
   {
       $this->middleware('auth');
+
+      $categories = Category::all();
+      View::share('categories',$categories);
   }
 
 public function index()
@@ -30,6 +35,7 @@ public function createAnnouncement(AnnouncementRequest $request)
     $a->title = $request->input('title');
     $a->body = $request->input('body');
     $a->category_id = $request->input('category');
+    $a->price = $request->input('price');
     $a->save();
     return redirect()->route('home')->with('announcement.create.success','Anuncio creado con exito');
 }
--- resources/views/announcement/new.blade.php
+++ resources/views/announcement/new.blade.php
@@ -8,45 +8,54 @@
                     Nuevo anuncio
                 </div>
                 <div class="card-body">
+                    <form method="POST" action='{{route("announcement.create")}}'>
+                        @csrf
+                        <div class="form-group">
+                            <label for="categories">Categorias</label>
+                            <select class="form-control" id="categories" name="category">
+                                @foreach ($categories as $category)
+                                <option value="{{$category->id}}" {{old('category') == $category->id ? 'selected' : ''}}>{{$category->name}}
+                                </option>
+                                @endforeach
+                            </select>
+                            @error('category')
+                                <span class="invalid-feedback" roles="alert"><strong>{{$message}}</strong></span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="announcementeName">Titulo</label>
+                            <input type="text" class="form-control" id="announcementeName" aria-describedby="emailHelp" name="title"
+                                value="{{old("title")}}">
+                            @error('title')
+                            <small id="emailHelp" class="form-text" style="color:red;">
+                                {{ $message }}
+                            </small>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="announcementeBody">Anuncio</label>
+                            <textarea class="form-control" name="body" id="announcementeBody" cols="30" rows="10">{{old("body")}}</textarea>
+                            @error('body')
+                            <small id="emailHelp" class="form-text" style="color:red;">
+                                {{ $message }}
+                            </small>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="announcementPrice">Precio</label>
+                            <input type="number" step="0.01" class="form-control" id="announcementPrice" aria-describedby="priceHelp" name="price" value="{{old("price")}}">
+                            @error('price')
+                            <small id="priceHelp" class="form-text" style="color:red;">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
 
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-
-<form method="POST" action='{{route("announcement.create")}}'>
-    @csrf
-    <div class="form-group">
-        <label for="categories">Categorias</label>
-        <select class="form-control" id="categories" name="category">
-            @foreach ($categories as $category)
-            <option value="{{$category->id}}" {{old('category') == $category->id ? 'selected' : ''}}>{{$category->name}}
-            </option>
-            @endforeach
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="announcementeName">Titulo</label>
-        <input type="text" class="form-control" id="announcementeName" aria-describedby="emailHelp" name="title"
-            value="{{old("title")}}">
-        @error('title')
-        <small id="emailHelp" class="form-text" style="color:red;">
-            {{ $message }}
-        </small>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="announcementeBody">Anuncio</label>
-        <textarea class="form-control" name="body" id="announcementeBody" cols="30" rows="10">{{old("body")}}</textarea>
-        @error('body')
-        <small id="emailHelp" class="form-text" style="color:red;">
-            {{ $message }}
-        </small>
-        @enderror
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
 
 @endsection
