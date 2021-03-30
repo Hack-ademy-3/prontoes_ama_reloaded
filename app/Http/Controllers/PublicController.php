a9d3233d55d01c7b2fa29fe1186057082395c108
Thunderstacker <sagradafuerza@gmail.com>
2021-03-30 19:24:19 +0200

    User Story 3 punto 7

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -15,10 +15,29 @@ class PublicController extends Controller
     return view('welcome', compact('announcements'));
     }
 
-    public function announcementsByCategory($name, $category_id)
+  /*   public function announcementsByCategory($name, $category_id)
     {
         $category = Category::find($category_id);
         $announcements = $category->announcements()->orderBy('created_at','desc')->paginate(5);
         return view('announcements', compact('category', 'announcements'));
-    }
+    } */
+    
+
+        public function index() {
+            $announcements = Announcement::where('is_accepted', true)
+                ->orderBy('created_at', 'desc')
+                ->take(5)
+                ->get();
+            return view('welcome', compact('announcements'));
+        }
+
+
+        public function announcementsByCategory($name, $category_id) {
+            $category = Category::find($category_id);
+            $announcements = $category->announcements()
+            ->where('is_accepted', true)
+            ->orderBy('created_at', 'desc')
+            ->paginate(5);
+            return view('announcements', compact('category', 'announcements'));
+}
 }
